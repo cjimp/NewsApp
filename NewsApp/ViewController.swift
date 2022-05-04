6a042ed161dc626df18be87e941f1e45b3fdd02d
cjimp <chajingui18@gmail.com>
2022-05-04 18:40:15 +0800

    项目初始化 && 添加.gitignore文件

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by pc on 2022/5/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
